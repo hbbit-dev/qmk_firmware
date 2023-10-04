c7332a15994b4c101efec951ca1b1a5ce131fb40
bactaholic <social@dbuckles.me>
2023-10-04 15:05:41 -0700

    fixed key assignments, fixed macros, updated TD functions

--- keyboards/moonlander/keymaps/bactaholic/keymap.c
+++ keyboards/moonlander/keymaps/bactaholic/keymap.c
@@ -35,12 +35,11 @@ enum {
     TD_TAB, //bactaholic - TAB + CAPS
     TD_SIGN, //bactaholic - KC_SIGN
     TD_PIPE, //bactaholic - add pipe, mostly for grep lol
+    TD_QUIT, //bactaholic - double tap to alt+f4
 };
 
 enum custom_keycodes {
-    VRSN = SAFE_RANGE,
-    KC_SIGN, //bactaholic - Create custom signature keycode
-    KC_QUIT, //bactaholic - Create custom quit keycode
+    EXAMPLE = SAFE_RANGE, //bactaholic - example custom keycode
     SELWORD, //bactaholic - Add SELWORD to keymap
 };
 
@@ -48,51 +47,66 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
         TD(TD_ESC) ,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_VOLU,           KC_PGUP,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        TD(TD_TAB) ,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(SYMB),         TG(CTRL),  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD_PIPE,
+        TD(TD_TAB) ,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(SYMB),         TG(CTRL),  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD(TD_PIPE),
         KC_LSFT,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    TD(TD_SIGN),           KC_PGDN,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_HOME,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_DEL,
-        KC_LCTL,  KC_LALT, KC_INS,  KC_LBRC, KC_RBRC,         KC_QUIT,           KC_MPLY,   KC_UP,   KC_DOWN, KC_LEFT, KC_RGHT, KC_RCTL,
+        KC_LCTL,  KC_LALT, KC_INS,  KC_LBRC, KC_RBRC,         TD(TD_QUIT),           KC_MPLY,   KC_UP,   KC_DOWN, KC_LEFT, KC_RGHT, KC_RCTL,
                                             KC_SPC,  KC_BSPC, KC_LGUI,           KC_EQL ,   KC_MINS,  KC_ENT
     ),
 
     [SYMB] = LAYOUT_moonlander(
         TD_ESC ,  KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_VOLU,           KC_PGUP, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        TD_TAB ,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(BASE),         TG(CTRL),  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD_PIPE,
-        KC_LSFT,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_SIGN,           KC_PGDN,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        TD_TAB ,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(SYMB),         TG(CTRL),  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD_PIPE,
+        KC_LSFT,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    TD(TD_SIGN),           KC_PGDN,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_HOME,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_DEL,
-        KC_LCTL,  KC_LALT, KC_INS,  KC_LBRC, KC_RBRC,         KC_QUIT,           KC_MPLY,  KC_UP, KC_DOWN, KC_LEFT, KC_RGHT, KC_RCTL,
+        KC_LCTL,  KC_LALT, KC_INS,  KC_LBRC, KC_RBRC,         TD(TD_QUIT),           KC_MPLY,  KC_UP, KC_DOWN, KC_LEFT, KC_RGHT, KC_RCTL,
                                             KC_SPC,  KC_BSPC, KC_LGUI,           KC_EQL ,  KC_MINS, KC_ENT
     ),
 
     [CTRL] = LAYOUT_moonlander(
         TD_ESC ,   KC_F1, KC_F2,   KC_F3  , KC_F4,   KC_F5,    KC_VOLU,           KC_PGUP, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______, RGB_MOD, _______, _______, _______, _______,  TG(SYMB),         TG(BASE), _______, _______,  _______,  _______,  _______, _______,
-        _______, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, _______,  KC_SIGN,           KC_PGDN, _______, _______,  _______,  _______,  _______, _______,
+        _______, RGB_MOD, _______, _______, _______, _______,  TG(SYMB),         TG(CTRL), _______, _______,  _______,  _______,  _______, _______,
+        _______, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, _______,  TD(TD_SIGN),           KC_PGDN, _______, _______,  _______,  _______,  _______, _______,
         _______, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, _______,                              _______, _______,  _______,  _______,  _______, _______,
         _______,  _______, _______, _______, _______,          _______,           _______,          _______,  _______,  _______,  _______,  _______,
                                             _______, _______, _______,            _______, _______, _______
     ),
 };
 
+void send_signature(tap_dance_state_t *state, void *user_data)
+{
+    if(state->count == 1) {
+        SEND_STRING(SS_TAP(X_VOLD));
+    } else {
+        SEND_STRING("bactaholic - ");
+    }
+}
+
+void send_quit(tap_dance_state_t *state, void *user_data)
+{
+    if(state->count == 1) {
+        SEND_STRING(SS_TAP(X_HOME)SS_DOWN(X_LSFT)SS_TAP(X_END)SS_UP(X_LSFT));
+    } else {
+        SEND_STRING(SS_DOWN(X_LALT)SS_TAP(X_F4)SS_UP(X_LALT));
+    }
+}
+
 tap_dance_action_t tap_dance_actions[] = {
-    [TD_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_TILD),
+    [TD_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_GRV),
     [TD_TAB] = ACTION_TAP_DANCE_DOUBLE(KC_TAB, KC_CAPS),
-    [TD_SIGN] = ACTION_TAP_DANCE_DOUBLE(KC_VOLD, KC_SIGN),
+    [TD_SIGN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, send_signature, NULL),
     [TD_PIPE] = ACTION_TAP_DANCE_DOUBLE(KC_BSLS, KC_PIPE),
+    [TD_QUIT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, send_quit, NULL),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-        switch (keycode) {
-            case KC_SIGN:
-                SEND_STRING("bactaholic - ");
-                return false;
-                break;
-            case KC_QUIT:
+    switch (keycode) {
+        case EXAMPLE:
+            if (record->event.pressed) {
                 SEND_STRING(SS_DOWN(X_LALT)SS_TAP(X_F4));
                 return false;
-                break;
-        }
+            }
+            break;
     }
     return true;
 }
